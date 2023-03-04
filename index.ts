4e36bb7285bbbabdd2be2ae7b4df56d66ec17791
Daniel Burger <public@danielburger.online>
2023-03-04 17:45:19 +0100

    Nearly having a working PDF reader

--- index.ts
+++ index.ts
@@ -1,5 +1,26 @@
 import { getCompletionStream } from 'https://deno.land/x/openai_chat_stream@1.0.0/mod.ts';
 import 'https://deno.land/x/dotenv@v3.2.2/load.ts';
+import {
+  loadpdfInClient,
+  parsepdfpage,
+} from 'https://deno.land/x/pdf_parser@v1.1.2/main.js';
+
+// Load the pdf.js library
+import pdfjsLib from 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.11.338/pdf.min.js';
+
+const pdfBytes = await Deno.readFile('example-paper.pdf');
+// convert the pdf to base64
+const pdfBase64 = btoa(String.fromCharCode(...pdfBytes));
+
+// Pass the pdfjsLib instance to loadpdfInClient
+const pdf = await loadpdfInClient(pdfBase64, pdfjsLib);
+
+const numPages = pdf.catalog.Pages.Count;
+
+for (let pageNum = 1; pageNum <= numPages; pageNum++) {
+  const page = await parsepdfpage(pdf, pageNum);
+  console.log(page.getTextContent());
+}
 
 const apiKeyArg = Deno.args.find((arg) => arg.startsWith('--api-key='));
 const apiKey = apiKeyArg
