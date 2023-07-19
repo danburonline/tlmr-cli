50737c97eacd5d776a7874205e7df616f3052d0e
Daniel Burger <public@danielburger.online>
2023-07-19 14:41:56 +0200

    Test LangChain and how it works
    Understand the concepts.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,36 @@
-import { OpenAI } from 'langchain/llms/openai'
+import { ChatOpenAI } from 'langchain/chat_models/openai'
+import { LLMChain } from 'langchain/chains'
+import {
+  ChatPromptTemplate,
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+} from 'langchain/prompts'
 
-const llm = new OpenAI({
-  temperature: 0.9,
+const template =
+  'You are a helpful assistant that translates {input_language} to {output_language}.'
+const systemMessagePrompt = SystemMessagePromptTemplate.fromTemplate(template)
+const humanTemplate = '{text}'
+const humanMessagePrompt =
+  HumanMessagePromptTemplate.fromTemplate(humanTemplate)
+
+const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+  systemMessagePrompt,
+  humanMessagePrompt,
+])
+
+const chat = new ChatOpenAI({
+  temperature: 0, // No randomness, be predictable
 })
 
-console.log(
-  await llm.predict(
-    'What would be a good company name for a company that makes colorful socks?'
-  )
-)
+const chain = new LLMChain({
+  llm: chat,
+  prompt: chatPrompt,
+})
+
+const result = await chain.call({
+  input_language: 'English',
+  output_language: 'French',
+  text: 'I love programming',
+})
+
+console.log(result)
